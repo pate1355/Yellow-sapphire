c65b2d02f7eec9fbfa17e9d41a2ad6a6e48f3405
RIKIN K PATEL <144366591+pate1355@users.noreply.github.com>
2025-04-03 22:44:54 -0400

    added favicon for meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,18 +17,57 @@ export const metadata = {
   title: "YELLOW SAPPHIRE",
   icons: {
     icon: [
-      { url: "/yellow-sapphire.ico", type: "image/x-icon" },
-      { url: "/YellowSapphire-icon.png", type: "image/png" },
+      {
+        url: "/favicon-for-public/favicon-16x16.png",
+        type: "image/png",
+        sizes: "16x16",
+      },
+      {
+        url: "/favicon-for-public/favicon-32x32.png",
+        type: "image/png",
+        sizes: "32x32",
+      },
+      {
+        url: "/favicon-for-public/favicon-192x192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: "/favicon-for-public/favicon-512x512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
     ],
-    shortcut: "/yellow-sapphire.ico",
-    apple: "/yellow-sapphire.ico",
+    shortcut: "/yellow-sapphire.ico", // classic .ico fallback
+    apple: "/favicon-for-public/apple-touch-icon.png", // for iOS home screen
   },
+
   description: "Created by DASHR Group",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon-for-public/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-for-public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-for-public/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/yellow-sapphire.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
