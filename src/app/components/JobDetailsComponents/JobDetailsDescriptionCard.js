78b6c0cbe071bb30ab7a94744c3062f73319ab61
RIKIN K PATEL <144366591+pate1355@users.noreply.github.com>
2025-04-10 18:00:00 -0400

    Enhances job details and layout consistency
    Improves layout responsiveness across job cards and description sections. Expands job details with additional fields like responsibilities, requirements, and benefits. Replaces icons with consistent library for sidebar. Updates bookmark icon colors for better contrast. Simplifies job overview extraction logic.
    
    Improves user experience and readability.

--- src/app/components/JobDetailsComponents/JobDetailsDescriptionCard.js
+++ src/app/components/JobDetailsComponents/JobDetailsDescriptionCard.js
@@ -3,10 +3,29 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export default function JobDetailsDescriptionCard({ job }) {
   return job && Object.keys(job).length > 0 ? (
-    <div className="bg-[#FFF2DE] md:w-full text-black  p-8 flex flex-col gap-4   rounded-[20px] shadow-sm">
-      <h2 className="text-xl font-semibold mb-2">Job Description</h2>
+    <div className="bg-[#FFF2DE] md:w-full text-black  p-8 flex flex-col gap-2   rounded-[20px] shadow-sm">
+      <h2 className="text-2xl font-semibold mb-2">Job Description</h2>
       <br />
-      <pre className="mb-6 text-wrap">{job?.job_description}</pre>
+      <pre className="mb-6 text-wrap">
+        {job?.job_about} {job?.job_overview}
+      </pre>
+      <h3 className="text-xl font-medium mb-3">Key Responsibilities</h3>
+      <pre className="mb-6 text-wrap">{job?.job_responsibilities}</pre>
+      <h3 className="text-xl font-medium mb-3">Key Requirements</h3>
+      <pre className="mb-6 text-wrap">{job?.job_requirements}</pre>
+      <h3 className="text-xl font-medium mb-3">Key Benefits</h3>
+      <pre className="mb-6 text-wrap">{job?.job_benefits}</pre>
+      <h3 className="text-xl font-medium mb-3">Tags</h3>
+      <div className="flex flex-wrap gap-2 mb-6">
+        {job?.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="bg-[#444444] text-white px-3 py-2 rounded-[12px] text-base"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
 
       <div className="flex items-center justify-start gap-6 mt-6">
         <p className="font-bold">Share Job:</p>
